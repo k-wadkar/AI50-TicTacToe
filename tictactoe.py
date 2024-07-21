3047c39891a13731e93f73ebb88501445fb7d2bb
Kshitij Wadkar <kshitijwadkar@hotmail.co.uk>
2024-07-21 18:54:01 +0100

    Winner function implemented

--- tictactoe.py
+++ tictactoe.py
@@ -71,8 +71,37 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Horizontal Check
+    for i in range(3):
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            if board[i][0] == "X":
+                return "X"
+            if board[i][0] == "O":
+                return "O"
+    
+    # Vertical Check
+    for i in range(3):
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            if board[0][i] == "X":
+                return "X"
+            if board[0][i] == "O":
+                return "O"
 
+    # Leading Diagonal Check
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        if board[1][1] == "X":
+            return "X"
+        if board[1][1] == "O":
+            return "O"
+
+    # Non-leading Diagonal Check
+    if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        if board[1][1] == "X":
+            return "X"
+        if board[1][1] == "O":
+            return "O"
+    
+    return None
 
 def terminal(board):
     """
